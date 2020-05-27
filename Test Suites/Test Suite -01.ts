145b72438f01fb6732bbca95992f63627b30ec36
Administrator <Administrator@irfan-PC>
2020-05-27 22:00:39 +1200

    New Test step Added

---
+++ Test Suites/Test Suite -01.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite -01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>273fcf51-ea9e-4f6f-b5bb-23a7592f0fcf</testSuiteGuid>
+   <testCaseLink>
+      <guid>d31ec0e4-93a4-4b04-850b-b56ac861a7d6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC001</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8f119850-ca48-4d70-bc07-11a36e30dd68</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC002</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
